bd91dc354fc709a0e9fb3525941eb53981c07705
Pedro Contine <pedro.contine@gmail.com>
2022-02-16 21:32:43 -0300

    Adding sample app

---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Pedro Contine on 16/02/22.
+//
+
+import UIKit
+import Treco
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(Treco().helloMessage())
+    }
+}
+
